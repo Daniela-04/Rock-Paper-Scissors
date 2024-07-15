b038ecfe92d815545a9f553ab4708dc7e2cecb7c
Daniela-04 <147045510+Daniela-04@users.noreply.github.com>
2024-07-15 04:30:14 +0200

    feature(eventos Onclick,cambiar fase)

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,22 +1,54 @@
-const rules = document.querySelector('#rules-btn');
-const imgRules=document.querySelector('#rules-img');
-const closeRules=document.querySelector('#close-rules')
+import menuReglas from './menuReglas.js';
+import winner from './winner.js';
+menuReglas();
+// funcion que gestiona el juego
+const game = () => {
+  let Usel;
+  let Hsel;
+  const opciones = document.querySelectorAll('#choose button');
+  // evento para escoger una opcion
 
+  // 🛠️ esto necesita otra vuelta, quiza volver al modelo de promesa
+  // 🛠️ y crear una cada que se clique un boton
+  opciones.forEach((option) => {
+    option.addEventListener('click', function () {
+      Usel = this;
+      // eliminar los botones deseleccionados
+      ocultarChoose();
+      // mostrar fase dos con la opcion del user solamente
+      mostrarWait(Usel);
 
-rules.addEventListener('click',()=>{
-    if(imgRules.style.display === "grid"){
-        imgRules.style.display = "none";
-        rules.style.display = "block";
-    } else {
-        imgRules.style.display = "grid";
-        rules.style.display = "none";
+      // escoger una opcion de la house y asignar la del usuraio
+      // 🛠️ la opcion de la house necesita un setimeout
+      Usel = Usel.id.charAt(0);
+      Hsel = houseSelection();
 
-    }
-    
-});
+      // mostrar quien gano el juego
+      const win = winner(Usel, Hsel);
+      console.log(win);
+    });
+  });
+};
 
-closeRules.addEventListener('click',()=>{
-    imgRules.style.display = "none";
-    rules.style.display = "block";
-});
+// funcion que escoge una opcion de la house
+const houseSelection = () => {
+  const opt = ['r', 'p', 's'];
+  const num = Math.floor(Math.random() * 3);
+  return opt[num];
+};
+// ocultar el contenedor de las opciones
+const ocultarChoose = () => {
+  const contChoose = document.getElementById('choose');
+  contChoose.style.display = 'none';
+};
+// mostrar el contenedor de espera
+const mostrarWait = (Usel) => {
+  const wait = document.getElementById('wait');
+  const selected = document.getElementById('selected');
 
+  wait.style.display = 'grid';
+  Usel.style.pointerEvents = 'none';
+  selected.append(Usel);
+};
+// inicializar el juego
+game();
