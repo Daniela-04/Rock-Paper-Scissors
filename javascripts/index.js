3a59d803f74e945bd7a4a28d40955591bced6651
Daniela-04 <147045510+Daniela-04@users.noreply.github.com>
2024-08-16 19:59:14 +0200

    refactor(index.js): reconstruye archivo desde cero con mejoras y un nuevo aproach

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,54 +1,108 @@
-import menuReglas from './menuReglas.js';
+import { handleGameChooseClick } from './uSel.js';
+import { houseSel, houseSelButton } from './houseSel.js';
+import { menuReglas } from './menuReglas.js';
 import winner from './winner.js';
-menuReglas();
-// funcion que gestiona el juego
-const game = () => {
-  let Usel;
-  let Hsel;
-  const opciones = document.querySelectorAll('#choose button');
-  // evento para escoger una opcion
+import { updateScore, getCookie } from './Score.js';
 
-  // 🛠️ esto necesita otra vuelta, quiza volver al modelo de promesa
-  // 🛠️ y crear una cada que se clique un boton
-  opciones.forEach((option) => {
-    option.addEventListener('click', function () {
-      Usel = this;
-      // eliminar los botones deseleccionados
-      ocultarChoose();
-      // mostrar fase dos con la opcion del user solamente
-      mostrarWait(Usel);
+// inicializar el juego y añadir el boton del usuario
+const initGameChoose = () => {
+  const contenedorIni = document.querySelector('.game-choose');
+  const contenedorRes = document.querySelector('.game-choose__result-you');
+  let botonClickeado = null;
+  contenedorIni.addEventListener('click', (e) => {
+    botonClickeado = handleGameChooseClick(e);
+    if (botonClickeado !== null) {
+      hideOtherButtons();
+      const opcion = botonClickeado;
+      opcion.classList.add('game-choose__option--disabled');
+      opcion.style.animation = 'fadeIn 1s 1 forwards';
+      contenedorRes.appendChild(opcion);
+      animacionCarga();
+    }
+  });
+};
+// inicializar el contador
+const initCounter = () => {
+  const counter = document.querySelector('.score__points-number');
+  counter.textContent = getCookie('score') || 0;
+};
 
-      // escoger una opcion de la house y asignar la del usuraio
-      // 🛠️ la opcion de la house necesita un setimeout
-      Usel = Usel.id.charAt(0);
-      Hsel = houseSelection();
+// ocultar el resto de botones luego de que el usuario elija
+const hideOtherButtons = () => {
+  const contenedor = document.querySelector('.game-choose');
+  contenedor.style.display = 'none';
+};
 
-      // mostrar quien gano el juego
-      const win = winner(Usel, Hsel);
-      console.log(win);
-    });
+// esperar a que la maquina elija
+const animacionCarga = () => {
+  const carga = document.querySelector('.carga');
+  const circle1 = document.querySelector('.carga__circle1');
+  carga.style.animation = 'fadeIn 1s 1 forwards';
+  circle1.style.animation = 'pulse 3s 3';
+  circle1.addEventListener('animationend', () => {
+    carga.style.animation = 'fadeOut 1s 1 forwards';
+  });
+  showResultCont();
+};
+
+// mostrarn contenedor de resultados y agregar boton de la maquina
+
+const showResultCont = () => {
+  const contenedor = document.querySelector('.game-choose__result');
+  contenedor.style.display = 'flex';
+  setTimeout(addHouseSel, 4000);
+};
+
+const addHouseSel = () => {
+  const resultCont = document.querySelector('.game-choose__result-house');
+  const carga = document.querySelector('.carga');
+  const hSel = houseSel();
+  const button = houseSelButton(hSel);
+
+  if (button !== null) {
+    setTimeout(() => {
+      if (carga) {
+        resultCont.removeChild(carga);
+      }
+      button.style.animation = 'fadeIn 1s 1 forwards';
+      resultCont.appendChild(button);
+    }, 500);
+  }
+  setTimeout(handlewin, 1000);
+};
+
+// ver quien gano, actualizar el puntaje en consecuencia
+
+const handlewin = () => {
+  let user = document.querySelector('.game-choose__result-you > button').getAttribute('id');
+  user = user.charAt(0);
+  const house = document.querySelector('.game-choose__result-house > button').getAttribute('data-option');
+  winner(user, house);
+  updateScore();
+  setTimeout(addPlayAgainButton, 1000);
+};
+
+//  añadir boton de volver a jugar
+
+const addPlayAgainButton = () => {
+  const contenedor = document.querySelector('.game-choose__result-center');
+  const button = document.createElement('button');
+  button.classList.add('button', 'game-choose__result-btn');
+  button.textContent = 'PLAY AGAIN';
+  button.style.animation = 'fadeIn 1s 1 forwards';
+  contenedor.appendChild(button);
+
+  button.addEventListener('click', () => {
+    window.location.reload();
   });
 };
 
-// funcion que escoge una opcion de la house
-const houseSelection = () => {
-  const opt = ['r', 'p', 's'];
-  const num = Math.floor(Math.random() * 3);
-  return opt[num];
-};
-// ocultar el contenedor de las opciones
-const ocultarChoose = () => {
-  const contChoose = document.getElementById('choose');
-  contChoose.style.display = 'none';
-};
-// mostrar el contenedor de espera
-const mostrarWait = (Usel) => {
-  const wait = document.getElementById('wait');
-  const selected = document.getElementById('selected');
+// main function
 
-  wait.style.display = 'grid';
-  Usel.style.pointerEvents = 'none';
-  selected.append(Usel);
-};
-// inicializar el juego
-game();
+function main () {
+  menuReglas();
+  initCounter();
+  initGameChoose();
+}
+
+main();
